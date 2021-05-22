5b2d2fce2c66ad41dedfa27462795a6d1cdbe440
nimiyaangel <60498459+nimiyaangel@users.noreply.github.com>
2021-05-22 23:25:34 +0530

    Add files via upload

---
+++ action.js
@@ -0,0 +1,12 @@
+function purple()
+{
+ document.body.style.background="purple";
+}
+function green()
+{
+    document.body.style.background="green";
+}
+function yellow()
+{
+    document.body.style.background="yellow";
+}
